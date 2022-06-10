c621bb13bd65fe0d5f065a0f52d254333ebb5ca3
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-10 13:53:44 -0700

    created questions for quiz

---
+++ js/questions.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "What will the following code return: Boolean(10 > 9)?",
+        choices: ["true", "false", "undefined", "null"],
+        answer: "true"
+    },
+    {
+        title: "How do you declare a JavaScript variable?",
+        choices: ["var myName = ", "var = myName", "var myName", "None of the above"],
+        answer: "var myName ="
+    },
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseclick", "onclick", "onmouse", "onchange"],
+        answer: "onclick"
+    },
+    {
+        title: "How do you add a comment in JavaScript?",
+        choices: ["// I am a comment", "I am a comment", "/* I am a comment */", "None of the above"],
+        answer: "// I am a comment"
+    },
+    {
+        title: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["round(7.25)", "Math.rnd(7.25)", "Math.round(7.25)", "round(7.25)"],
+        answer: "Math.round(7.25)"
+    }
+];
+
